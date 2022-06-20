6ccb4ac479f84b8e0ba47b34ea690bba0fae138e
Dmitry Kirsanov <dmitkirsanov@yandex.ru>
2022-06-20 19:24:26 +0300

    09 change user model

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'bio', 'role')
+    list_display_links = ('id', 'username',)
+    search_fields = ('username', 'email')
+
+admin.site.register(User, UserAdmin)
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    
+    user = 'u'
+    moderator = 'm'
+    admin = 'a'
+
+    roles = [
+    (user, 'user'),
+    (moderator, 'moderator'),
+    (admin, 'admin'),
+    ]
+
+
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+        max_length=100,
+    )
+    role = models.CharField(
+        max_length=1,
+        blank=False,
+        choices=roles,
+        default=user,
+    )
+
+
