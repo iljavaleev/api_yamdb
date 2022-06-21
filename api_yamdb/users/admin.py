6e8eb34b6fb14d101f9aed667910426f4db495ea
Dmitry Kirsanov <dmitkirsanov@yandex.ru>
2022-06-21 09:04:31 +0300

    18

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,23 +1,14 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-from .models import User
-=======
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
->>>>>>> 5ca4f1f0c443ec0e28d06357e363acc8cdfc9e90
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'bio', 'role')
     list_display_links = ('id', 'username',)
     search_fields = ('username', 'email')
 
-<<<<<<< HEAD
-admin.site.register(User, UserAdmin)
-=======
 
 admin.site.register(User, UserAdmin)
->>>>>>> 5ca4f1f0c443ec0e28d06357e363acc8cdfc9e90
