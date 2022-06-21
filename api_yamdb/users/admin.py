3ff806df4684d5e52b6d7ab731c49fb6e1ed04de
Ilya Valeev <iljavaleev@gmail.com>
2022-06-21 13:38:31 +0300

    add to req

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -6,7 +6,15 @@ User = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'bio', 'role')
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'bio',
+        'role'
+    )
     list_display_links = ('id', 'username',)
     search_fields = ('username', 'email')
 
