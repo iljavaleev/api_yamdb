27b1772811007d0a0fafec6f7db3b38f448454e4
Dmitry Kirsanov <dmitkirsanov@yandex.ru>
2022-06-23 10:27:30 +0300

    35

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -122,57 +122,62 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only_fields = ('post',)
 
 
-class SignupUserSerializer(serializers.Serializer):
+# class SignupUserSerializer(serializers.Serializer):
 
-    username = serializers.CharField(
-        validators=(UniqueValidator(queryset=User.objects.all()),)
-    )
-    email = serializers.EmailField(
-        validators=(UniqueValidator(queryset=User.objects.all()),)
-    )
+#     username = serializers.CharField(
+#         validators=(UniqueValidator(queryset=User.objects.all()),)
+#     )
+#     email = serializers.EmailField(
+#         validators=(UniqueValidator(queryset=User.objects.all()),)
+#     )
 
+#     class Meta:
+#         validators = (
+#             serializers.UniqueTogetherValidator(
+#                 queryset=User.objects.all(),
+#                 fields=('username', 'email'),
+#                 message='Обязательные поля'
+#             ),
+#         )
+
+#     def validate(self, data):
+#         if data['username'] == 'me':
+#             raise serializers.ValidationError('"me" — запретное имя пользователя')
+#         return data
+
+#     def create(self, validated_data):
+#         return User.objects.create(**validated_data)
+
+        
+
+
+
+
+
+class SignupUserSerializer(serializers.ModelSerializer):
+
+    username = serializers.CharField(max_length=150)
+    email = serializers.EmailField(max_length=254)
 
     class Meta:
-        validators = (
-            serializers.UniqueTogetherValidator(
-                queryset=User.objects.all(),
-                fields=('username', 'email'),
-                message='Обязательные поля'
-            ),
-        )
+        model = User
+        fields = ['username', 'email']
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
 
     def validate(self, data):
-        if data['username'] == 'me':
-            raise serializers.ValidationError('"me" — запретное имя пользователя')
+        user = get_object_or_404(User, username=data['username'])
+
+        if user.username == 'me':
+            raise serializers.ValidationError(
+                '"me" — запретное имя пользователя'
+            )
+        elif User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError('Такое имя пользователя уже существует')
+        elif User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError('Такой email уже существует')
         return data
-
-
-
-
-
-# class SignupUserSerializer(serializers.Serializer):
-
-#     username = serializers.CharField()
-#     email = serializers.EmailField()
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email']
-
-#     def create(self, validated_data):
-#         return User.objects.create(**validated_data)
-
-#     def validate(self, data):
-#         user = get_object_or_404(User, username=data['username'])
-
-#         if user.username == 'me':
-#             raise serializers.ValidationError(
-#                 '"me" — запретное имя пользователя'
-#             )
-#         return data
 
 
 
