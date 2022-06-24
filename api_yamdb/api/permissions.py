3c4d60c1b2ca5ad8b7df68619bdaecf47d4161b0
iljavaleev <iljavaleev@gmail.com>
2022-06-24 15:43:28 +0300

    anotherone

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -21,31 +21,29 @@ class SignupUserPermission(permissions.BasePermission):
 class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS or obj.token
+        return (request.method in permissions.SAFE_METHODS or obj.author
                 == request.user)
 
 
 class IsModeratorPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.role == User.MODERATOR
-        )
+        return request.user.role == User.MODERATOR
+
 
 class IsAdminPermission(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        return (
-            # request.method in permissions.SAFE_METHODS
-            # or request.user.is_authenticated
-            request.user.is_authenticated
-        )
+    # def has_permission(self, request, view):
+    #     return (
+    #         # request.method in permissions.SAFE_METHODS
+    #         # or request.user.is_authenticated
+    #         request.user.is_authenticated
+    #     )
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.user.is_staff
-            or request.user.is_superuser
+                request.user.role == User.ADMIN
+                or request.user.is_superuser == True
         )
 
 class IsAuthenticatedPermission(permissions.BasePermission):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -85,13 +85,12 @@ class ReviewViewSet(viewsets.ModelViewSet):
         )
 
     def get_permissions(self):
-        if self.action == 'delete':
-            permission_classes = [
-                IsAuthorOrReadOnly|IsModeratorPermission|IsAdminPermission]
-        elif  self.action == 'update':
+        if self.action in ['destroy','update','partial_update']:
             permission_classes = [
                 IsAuthorOrReadOnly | IsModeratorPermission | IsAdminPermission]
-        permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticatedOrReadOnly]
+
 
         return [permission() for permission in permission_classes]
 
@@ -113,13 +112,11 @@ class CommentViewSet(viewsets.ModelViewSet):
                         )
 
     def get_permissions(self):
-        if self.action == 'delete':
+        if self.action in ['destroy', 'update', 'partial_update']:
             permission_classes = [
                 IsAuthorOrReadOnly | IsModeratorPermission | IsAdminPermission]
-        elif self.action == 'update':
-            permission_classes = [
-                IsAuthorOrReadOnly | IsModeratorPermission | IsAdminPermission]
-        permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAuthenticatedOrReadOnly]
 
         return [permission() for permission in permission_classes]
 
