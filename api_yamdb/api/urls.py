56be2ea1f6eee7d688ad9c8bc23d94ebceb55b6a
iljavaleev <iljavaleev@gmail.com>
2022-06-26 15:31:06 +0300

    clean

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -14,12 +14,12 @@ router.register('titles', TitlesViewSet, basename='titles')
 router.register(r'titles/(?P<title_id>[0-9]+)/reviews', ReviewViewSet,
                 basename='reviews')
 router.register(
-     r'titles/(?P<title_id>[0-9]+)/reviews/(?P<review_id>[0-9]+)/comments',
-     CommentViewSet,
-     basename='comments'
+    r'titles/(?P<title_id>[0-9]+)/reviews/(?P<review_id>[0-9]+)/comments',
+    CommentViewSet,
+    basename='comments'
 )
 urlpatterns = [
-     path('v1/', include(router.urls)),
-     path('v1/auth/signup/', SignupUser),
-     path('v1/auth/token/', TokenUser)
+    path('v1/', include(router.urls)),
+    path('v1/auth/signup/', SignupUser),
+    path('v1/auth/token/', TokenUser)
 ]
